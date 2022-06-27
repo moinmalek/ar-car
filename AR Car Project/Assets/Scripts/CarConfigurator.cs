81b33f1089b26656713d3a2bd6d22d876f575d50
Moin Malek <moinrobotics@gmail.com>
2022-06-27 14:59:47 +0200

    CarConfigurator updated & unnecessary assets removed

--- AR Car Project/Assets/Scripts/CarConfigurator.cs
+++ AR Car Project/Assets/Scripts/CarConfigurator.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class CarConfigurator : MonoBehaviour
 {
+    //define different properties
+    List<string> colors = new List<string>() { "Blue", "Red", "Green", "Yellow" };
+    List<string> engines = new List<string>() { "Engine1", "Engine2" };
+    List<string> steerings = new List<string>() { "Ackerman", "Anti-Ackerman" };
+    List<string> suspensions = new List<string>() { "Semi-dependent", "Independent" };
+    List<string> spoilers = new List<string>() { "Without", "Rear-Wing" };
+    List<string> coolings = new List<string>() { "Radiator1", "Radiator2" };
+    List<string> turbos = new List<string>() { "Single", "Double" };   
+    
     void Start()
     {
+        //all variants together 
+        List<List<string>> variants = new List<List<string>>() { colors, engines, steerings, suspensions, spoilers, coolings, turbos };
         
+        foreach (List<string> variant in variants)
+        {
+            foreach(string property in variant)
+            { 
+                Debug.Log(property); 
+            }
+        }
     }
 
     void Update()
@@ -18,19 +36,44 @@ public class CarConfigurator : MonoBehaviour
     //input params = what user selects
     //manually name the Text exactly same as variant name
     //return it to show parts and animation
-    
+    public Car ReturnCar()
+    {
+        Car car = new Car();
+        return car;
+    }
+
     //method which takes in class car from above method & returns animation/hide-unhide of objects
     //drive display etc of car object
 
+}
 
-    class Car
+public class Car
+{
+    public string Color { get; set; }
+    public string Engine { get; set; }
+    public string Suspension { get; set; }
+    public string Steering { get; set; }
+    public string Spoiler { get; set; }
+    public string Cooling { get; set; }
+    public string Turbo { get; set; }
+
+    //constructor with all properties
+    public Car(string color, string engine, string suspension, string steering, string spoiler, string cooling, string turbo)
     {
-        public string color, engine, suspension, steering, spoiler, cooling;
-        public int turbo;
+        color = Color;
+        engine = Engine;
+        suspension = Suspension;
+        steering = Steering;
+        spoiler = Spoiler;
+        cooling = Cooling;
+        turbo = Turbo;
+    }
 
-        public Car()
-        {
+    //default constructor
+    public Car()
+    {
 
-        }
     }
 }
+
+
